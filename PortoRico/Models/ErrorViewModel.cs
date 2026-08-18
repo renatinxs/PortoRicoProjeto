e9f756ad96b6aec30d35a4589a564d8ee0b1006f
Renato Oliveira <Renato Oliveira>
2026-08-18 09:16:25 -0300

    Iniciando projeto

---
+++ PortoRico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortoRico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
